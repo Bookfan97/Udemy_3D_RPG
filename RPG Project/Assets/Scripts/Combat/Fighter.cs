537170253565a176bda7e09c0d257ea0fcc6da86
Noah <sheltielover432@gmail.com>
2020-02-15 14:17:39 -0500

    Dealing Damage

--- RPG Project/Assets/Scripts/Combat/Fighter.cs
+++ RPG Project/Assets/Scripts/Combat/Fighter.cs
@@ -8,6 +8,7 @@ namespace RPG.Combat
     {
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
         Transform target;
         float timeSinceLastAttack = 0;
         private void Update()
@@ -32,9 +33,15 @@ namespace RPG.Combat
             {
                 GetComponent<Animator>().SetTrigger("attack");
                 timeSinceLastAttack = 0;
+               //This will trigger the Hit() event
             }
         }
-
+        //Animation Event
+        void Hit()
+        {
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
+        }
         private bool GetIsInRange()
         {
             return Vector3.Distance(transform.position, target.position) < weaponRange;
@@ -47,11 +54,6 @@ namespace RPG.Combat
         public void Cancel()
         {
             target = null;
-        }
-        //Animation Event
-        void Hit()
-        {
-            
         }
     }
 }
