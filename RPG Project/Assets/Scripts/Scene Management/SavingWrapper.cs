bd7b3c5492ca8f11bb0a1e7848691f4672c24fdb
Noah <sheltielover432@gmail.com>
2020-02-18 21:20:01 -0500

    Saving Wrapper

---
+++ RPG Project/Assets/Scripts/Scene Management/SavingWrapper.cs
@@ -0,0 +1,37 @@
+﻿using RPG.Saving;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaultSaveFile = "save";
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+        }
+
+        private void Save()
+        {
+            GetComponent<SavingSystem>().Save(defaultSaveFile);
+        }
+
+        private void Load()
+        {
+            //call to saving system
+            GetComponent<SavingSystem>().Load(defaultSaveFile);
+        }
+
+    }
+}
