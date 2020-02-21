c16aaae5e8907b1fd8bc42bd4537cccd53cf975a
Noah <sheltielover432@gmail.com>
2020-02-20 20:53:28 -0500

    Creating Scriptable Objects

---
+++ RPG Project/Assets/Scripts/Combat/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace RPG.Combat
+{
+    [CreateAssetMenu(fileName = "weapon", menuName = "Weapons/Make New Weapom", order = 0)]
+    public class Weapon : ScriptableObject
+    {
+
+        [SerializeField] AnimatorOverrideController animatorOverride = null;
+        [SerializeField] GameObject weaponPrefab = null;
+
+        public void Spawn(Transform handTransform, Animator animator)
+        {
+            Instantiate(weaponPrefab, handTransform);
+            animator.runtimeAnimatorController = animatorOverride;
+        }
+    }
+}
