9cbd205b3573add36f69457f14f0f3662c715937
Noah <sheltielover432@gmail.com>
2020-02-22 21:20:17 -0500

    Arrows

--- RPG Project/Assets/Scripts/Combat/Fighter.cs
+++ RPG Project/Assets/Scripts/Combat/Fighter.cs
@@ -68,9 +68,20 @@ namespace RPG.Combat
         void Hit()
         {
             if (target == null) { return; }
-            target.TakeDamage(defaultWeapon.GetWeaponDamage());
+            if (currentWeapon.HasProjectile())
+            {
+                currentWeapon.LaunchProjectile(rightHandTransform, leftHandTransform, target);
+            }
+            else
+            {
+                target.TakeDamage(defaultWeapon.GetWeaponDamage());
+            }
         }
-
+        // Animation Event
+        void Shoot()
+        {
+            Hit();
+        }
         private bool GetIsInRange()
         {
             return Vector3.Distance(transform.position, target.transform.position) < currentWeapon.GetWeaponRange();
