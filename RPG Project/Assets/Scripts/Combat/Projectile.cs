8ed1d7ed2840062260d73cc913fc034718b85afb
Noah <sheltielover432@gmail.com>
2020-02-22 23:51:21 -0500

    Fireball

--- RPG Project/Assets/Scripts/Combat/Projectile.cs
+++ RPG Project/Assets/Scripts/Combat/Projectile.cs
@@ -8,6 +8,7 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] float speed = 1;
     [SerializeField] bool isHoming = true;
+    [SerializeField] GameObject hitEffect = null;
     Health target = null;
     float damage = 0;
     private void Start()
@@ -45,6 +46,10 @@ public class Projectile : MonoBehaviour
         if (other.GetComponent<Health>() != target) return;
         if (target.IsDead()) return;
         target.TakeDamage(damage);
+        if(hitEffect != null)
+        {
+            Instantiate(hitEffect,GetAimLocation(), transform.rotation);
+        }
         Destroy(gameObject);
     }
 }
