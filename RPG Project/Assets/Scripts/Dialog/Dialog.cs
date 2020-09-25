c1d73285dda25237184ae9009c3e3afd4e67fd82
Noah Day <sheltielover432@gmail.com>
2020-09-24 20:44:38 -0400

    Scriptable Objects

---
+++ RPG Project/Assets/Scripts/Dialog/Dialog.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dialog", menuName = "Dialog", order = 0)]
+public class Dialog : ScriptableObject
+{
+    [SerializeField] private DialogNode[] nodes;
+}
