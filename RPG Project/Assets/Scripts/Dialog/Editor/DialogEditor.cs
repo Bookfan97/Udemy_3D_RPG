d912fbb6ac63b9e3c428e75c33b99c559fcdd618
Noah Day <sheltielover432@gmail.com>
2020-09-24 21:13:24 -0400

    Update DialogEditor.cs

--- RPG Project/Assets/Scripts/Dialog/Editor/DialogEditor.cs
+++ RPG Project/Assets/Scripts/Dialog/Editor/DialogEditor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 namespace RPG.Dialog.Editor
@@ -11,6 +12,19 @@ namespace RPG.Dialog.Editor
         public static void ShowEditorWindow()
         {
             GetWindow(typeof(DialogEditor), false, "Dialogue Editor");
+        }
+
+        [OnOpenAssetAttribute(1)]
+        public static bool OnOpenAsset(int instanceID, int line)
+        {
+            Dialog dialog = EditorUtility.InstanceIDToObject(instanceID) as Dialog;
+            if (dialog != null)
+            {
+                //Debug.Log("Opening " + dialog);
+                ShowEditorWindow();
+                return true;
+            }
+            return false;
         }
     }
 }
