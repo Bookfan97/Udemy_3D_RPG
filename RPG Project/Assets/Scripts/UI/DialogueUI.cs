4596e2925b0eb5cd2e781a1928d328f0d2661458
Noah Day <sheltielover432@gmail.com>
2020-10-13 22:12:07 -0400

    Dialogue Architecture

---
+++ RPG Project/Assets/Scripts/UI/DialogueUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Dialogue;
+using TMPro;
+using UnityEngine;
+
+namespace RPG.UI
+{
+    public class DialogueUI : MonoBehaviour
+    {
+        private PlayerConversant playerConversant;
+        [SerializeField] private TextMeshProUGUI AIText;
+        // Start is called before the first frame update
+        void Start()
+        {
+            playerConversant = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConversant>();
+            AIText.text = playerConversant.GetText();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
