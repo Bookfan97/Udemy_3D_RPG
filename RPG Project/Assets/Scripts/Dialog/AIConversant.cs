bd6babee9ce4d0e04f4b2220bd075c73ba2eae46
Noah Day <sheltielover432@gmail.com>
2021-06-13 22:27:01 -0400

    Merge Fix

---
+++ RPG Project/Assets/Scripts/Dialog/AIConversant.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Control;
+using UnityEngine;
+
+namespace RPG.Dialogue
+{
+
+
+    public class AIConversant : MonoBehaviour, IRaycastable
+    {
+        [SerializeField] private Dialogue dialogue = null;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public CursorType GetCursorType()
+        {
+            return CursorType.Dialogue;
+        }
+
+        public bool HandleRaycast(PlayerController callingController)
+        {
+            if (dialogue == null) return false;
+            if (Input.GetMouseButtonDown(0))
+            {
+                callingController.GetComponent<PlayerConversant>().StartDialogue(this, dialogue);
+            }
+            return true;
+        }
+    }
+}
